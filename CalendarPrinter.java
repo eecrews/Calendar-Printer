7030d0348c1ae41ea036e456d6b09e24d5c9f832
Erin Crews <61025723+eecrews@users.noreply.github.com>
2021-09-14 13:08:20 -0500

    Remove personal information

--- CalendarPrinter.java
+++ CalendarPrinter.java
@@ -2,37 +2,7 @@
 //
 // Title: Calendar Printer
 // Files: CalendarPrinter.java, CalendarTester.java
-// Course: CS300, Term 1, 2019
-//
-// Author: Erin Crews
-// Email: eecrews@wisc.edu
-// Lecturer's Name: Gary Dahl
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:
-// Partner Email:
-// Partner Lecturer's Name:
-//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-// ___ Write-up states that pair programming is allowed for this assignment.
-// ___ We have both read and understand the course Pair Programming Policy.
-// ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully
-// acknowledge and credit those sources of help here. Instructors and TAs do
-// not need to be credited here, but tutors, friends, relatives, room mates,
-// strangers, and others do. If you received no outside help from either type
-// of source, then please explicitly indicate NONE.
-//
-// Persons: NONE
-// Online Sources: https://www.developer.com/java/data/understanding-the-java-labeled-statement.html
-// This online source helped me learn how to break out of a nested for loop in one statement using
-//////////////////// labels.
-//
-/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+// Course: CS300
 
 import java.util.Scanner;
 
